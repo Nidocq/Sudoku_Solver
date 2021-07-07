9ce2dab046283247170bacbbc09a4597200cfb34
Phillip Lundin <phillip.linnemann.lundin@gmail.com>
2021-07-07 18:20:56 +0200

    Added interactiveness

--- BackTrack.py
+++ BackTrack.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 from Board import *
+import time
+import os
+
 board = Board()
 
-def solve(row, col):
-    print("\nNew line")
-    print(f"{row=}", end='')
-    print(f"{col=}")
+def solve(row, col, progressive):
+    # print("\nNew line")
+    # print(f"{row=}", end='')
+    # print(f"{col=}")
     if (row == board.size - 1 and col == board.size):
         return True
 
@@ -15,13 +18,17 @@ def solve(row, col):
     
     for value in range(1, board.size+1):
         if board.grid[row][col] != 0:
-            return solve(row, col+1)
+            return solve(row, col+1, progressive)
 
         if board.validPlacement(row, col, value):
-            print("Value is {} and is being set".format(value))
             board.grid[row][col] = value
+            if progressive:
+                time.sleep(0.1)
+                os.system("clear")
+                board.showGrid()
+    
 
-            if solve(row, col+1):
+            if solve(row, col+1, progressive):
                 return True
 
         board.grid[row][col] = 0
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from Board import *
 from BackTrack import *
+import os 
 
-if solve(0, 0):
-    print("--- Winning ---")
+if solve(0, 0, True): #Enable this for progressive solution
+    os.system("clear")
     board.showGrid()
-
+    print("--- Winning ---")
 else:
     print("No solution was found")
-    board.showGrid()
 
     
